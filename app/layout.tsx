fb8eb8650b8ca2e75030911e5fa9d94eb286bc07
TomasLonnblad <tomaslonnblad@gmail.com>
2026-02-22 15:29:26 +0100

    x

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,8 +3,7 @@ import type { Metadata } from 'next';
 import { Inter, Silkscreen } from 'next/font/google';
 
 import './globals.css';
-import NavBar from '@/components/layout/NavBar';
-import Footer from '@/components/layout/Footer';
+
 
 const inter = Inter({
   subsets: ['latin'],
